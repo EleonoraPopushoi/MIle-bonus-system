1e9dd8f71f412d27c9e6c6224d4989b5cc2cf728
Eleonora Popushoi <eleonorapopushoi@gmail.com>
2020-08-23 15:28:35 +0300

    Mile's bonus system

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 15000;
+        int mile = 20;
+        int bonusinmoney = price/mile;
+        System.out.println(bonusinmoney);
+    }
+}
